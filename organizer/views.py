34354fb2fe62879cc97691f4e42077cc85f2e88d
Kevin Trainor <trainor1@illinois.edu>
2017-12-05 13:27:49 -0600

    add template code from 4.5.3 .

--- organizer/views.py
+++ organizer/views.py
@@ -1,3 +1,11 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Tag
+
+
+def homepage(request):
+    tag_list = Tag.objects.all()
+    output = ", ".join(tag.name for tag in tag_list)
+    return HttpResponse(output)
+
