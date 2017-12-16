375b0e924916392ffbad5948430c9972cb648397
Kevin Trainor <trainor1@illinois.edu>
2017-12-16 10:42:28 -0600

    add example 5.8 .

--- organizer/views.py
+++ organizer/views.py
@@ -12,3 +12,13 @@ def homepage(request):
     output = template.render(context)
     return HttpResponse(output)
 
+
+def tag_detail(request, slug):
+    # slug = ?
+    tag = Tag.objects.get(slug__iexact=slug)
+    template = loader.get_template(
+        'organizer/tag_detail.html')
+    context = Context({'tag': tag})
+    return HttpResponse(template.render(context))
+
+
