4612630179dacb83261b7b7dba8653ca1e49a574
Kevin Trainor <trainor1@illinois.edu>
2017-12-18 15:33:57 -0600

    add example 5.55 .

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Post
 
@@ -9,4 +9,16 @@ def post_list(request):
         'blog/post_list.html',
         {'post_list': Post.objects.all()})
 
+
+def post_detail(request, year, month, slug):
+    post = get_object_or_404(
+        Post,
+        pub_date__year=year,
+        pub_date__month=month,
+        slug=slug)
+    return render(
+        request,
+        'blog/post_list.html',
+        {'post': post}
+    )
 
