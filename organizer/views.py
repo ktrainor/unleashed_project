18b8d9368703cab4416cf30494fe007d2fa3e231
Kevin Trainor <trainor1@illinois.edu>
2017-12-18 10:45:59 -0600

    add example 5.23 .

--- organizer/views.py
+++ organizer/views.py
@@ -1,16 +1,14 @@
-from django.http.response import HttpResponse, Http404
+from django.http.response import HttpResponse
 from django.template import Context, loader
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render_to_response
 
 from .models import Tag
 
 
 def homepage(request):
-    tag_list = Tag.objects.all()
-    template = loader.get_template('organizer/tag_list.html')
-    context = Context({'tag_list': tag_list})
-    output = template.render(context)
-    return HttpResponse(output)
+    return render_to_response(
+        'organizer/tag_list.html',
+        {'tag_list': Tag.objects.all})
 
 
 def tag_detail(request, slug):
