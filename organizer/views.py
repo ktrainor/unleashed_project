68107f4ac9490fa46cd1638787c7d9f8e8456409
Kevin Trainor <trainor1@illinois.edu>
2017-12-18 10:34:53 -0600

    add example 5.20 .

--- organizer/views.py
+++ organizer/views.py
@@ -1,6 +1,6 @@
 from django.http.response import HttpResponse, Http404
 from django.template import Context, loader
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 from .models import Tag
 
@@ -14,10 +14,8 @@ def homepage(request):
 
 
 def tag_detail(request, slug):
-    try:
-        tag = Tag.objects.get(slug__iexact=slug)
-    except Tag.DoesNotExist:
-        raise Http404
+    tag = get_object_or_404(
+        Tag, slug__iexact=slug)
     template = loader.get_template(
         'organizer/tag_detail.html')
     context = Context({'tag': tag})
