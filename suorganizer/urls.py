1b4e01154a164f617d383193923fca0fd768489c
Kevin Trainor <trainor1@illinois.edu>
2017-12-18 16:28:28 -0600

    add example 5.76 .

--- suorganizer/urls.py
+++ suorganizer/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from .views import redirect_root
+
 from organizer import urls as organizer_urls
 from blog import urls as blog_urls
 
 urlpatterns = [
+    url(r'^$', redirect_root),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include(organizer_urls)),
     url(r'^blog/', include(blog_urls)),
-
 ]
