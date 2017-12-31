f204fe71697284cb3e8e4d306224c494402c8b1c
Kevin Trainor <trainork@uwm.edu>
2017-12-31 16:09:19 -0600

    add code from example 6.39 .

--- organizer/models.py
+++ organizer/models.py
@@ -40,6 +40,9 @@ class Startup(models.Model):
         ordering = ['name']
         get_latest_by = 'founded_date'
 
+    def get_absolute_url(self):
+        return reverse('organizer_startup_detail', kwargs={'slug': self.slug})
+
 
 class NewsLink(models.Model):
     title = models.CharField(max_length=63)
