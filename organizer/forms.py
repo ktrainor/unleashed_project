69036d3d4bd0ef822a2880912235d49802ff4864
Kevin Trainor <trainork@uwm.edu>
2018-01-01 14:44:14 -0600

    add code from example 7.16 .

--- organizer/forms.py
+++ organizer/forms.py
@@ -4,17 +4,10 @@ from django.core.exceptions import ValidationError
 from .models import Tag
 
 
-class TagForm(forms.Form):
-    name = forms.CharField(max_length=31)
-    slug = forms.SlugField(
-        max_length=31,
-        help_text='A label for URL config')
-
-    def save(self):
-        new_tag = Tag.objects.create(
-            name = self.cleaned_data['name'],
-            slug = self.cleaned_data['slug'])
-        return new_tag
+class TagForm(forms.ModelForm):
+    class Meta:
+        model = Tag
+        fields = '__all__'
 
     def clean_name(self):
         return self.cleaned_data['name'].lower()
