7bd39355e08c70516653d28e4f085592e69dea55
Kevin Trainor <trainork@uwm.edu>
2018-01-01 14:35:10 -0600

    add code from example 7.15 .

--- organizer/forms.py
+++ organizer/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from .models import Tag
 
@@ -17,3 +18,10 @@ class TagForm(forms.Form):
 
     def clean_name(self):
         return self.cleaned_data['name'].lower()
+
+    def clean_slug(self):
+        new_slug = self.cleaned_data['slug'].lower()
+        if new_slug == 'create':
+            raise ValidationError(
+                'Slug may not be "create".')
+        return new_slug
