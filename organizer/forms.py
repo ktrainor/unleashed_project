cbeb3d3f4f0d290b3105cbe0acbd04f85359c563
Kevin Trainor <trainork@uwm.edu>
2018-01-01 16:47:57 -0600

    add code from example 7.32 .

--- organizer/forms.py
+++ organizer/forms.py
@@ -10,10 +10,8 @@ class NewsLinkForm(forms.ModelForm):
         fields = '__all__'
 
 
-class StartupForm(forms.ModelForm):
-    class Meta:
-        model = Startup
-        fields = '__all__'
+class SlugCleanMixin:
+    """Mixin class fro slug cleaning method."""
 
     def clean_slug(self):
         new_slug = self.cleaned_data['slug'].lower()
@@ -23,7 +21,13 @@ class StartupForm(forms.ModelForm):
         return new_slug
 
 
-class TagForm(forms.ModelForm):
+class StartupForm(SlugCleanMixin, forms.ModelForm):
+    class Meta:
+        model = Startup
+        fields = '__all__'
+
+
+class TagForm(SlugCleanMixin, forms.ModelForm):
     class Meta:
         model = Tag
         fields = '__all__'
@@ -31,9 +35,4 @@ class TagForm(forms.ModelForm):
     def clean_name(self):
         return self.cleaned_data['name'].lower()
 
-    def clean_slug(self):
-        new_slug = self.cleaned_data['slug'].lower()
-        if new_slug == 'create':
-            raise ValidationError(
-                'Slug may not be "create".')
-        return new_slug
+
