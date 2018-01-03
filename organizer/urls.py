342ceebb9e2ffb97819bff82b200aba7e3d86f38
Kevin Trainor <trainork@uwm.edu>
2018-01-03 11:20:49 -0600

    fix import from example 9.29 .

--- organizer/urls.py
+++ organizer/urls.py
@@ -1,12 +1,16 @@
 from django.conf.urls import url
 
 from .views import (startup_list, startup_detail,
-                    tag_detail, tag_list, TagCreate)
+                    tag_detail, tag_list, StartupCreate, TagCreate)
+
 
 urlpatterns = [
     url(r'^startup/$',
         startup_list,
         name='organizer_startup_list'),
+    url(r'^startup/create/$',
+       StartupCreate.as_view(),
+       name = 'organizer_startup_create'),
     url(r'^startup/(?P<slug>[\w\-]+)/$',
         startup_detail,
         name='organizer_startup_detail'),
