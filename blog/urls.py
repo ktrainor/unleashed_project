19e147bfda4ad32f38df4355af41a3b3912be829
Kevin Trainor <trainork@uwm.edu>
2018-01-03 10:54:09 -0600

    fix import from example 9.26 .

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import url
 
-from .views import PostList, post_detail
-
+from .views import PostList, post_detail, PostCreate
 
 urlpatterns = [
     url(r'^$',
         PostList.as_view(),
         name='blog_post_list'),
+    url(r'^create/$',
+        PostCreate.as_view(),
+        name='blog_post_create'),
     url(r'^(?P<year>\d{4})/'
         r'(?P<month>\d{1,2})/'
         r'(?P<slug>[\w\-]+)/$',
